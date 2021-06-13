2b6d71d95d61567d67e1d177a3f9bb8856536ddf
Will Bowe <willjbowe@gmail.com>
2021-06-13 11:36:14 -0700

    Updated if statment

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -5,4 +5,32 @@ function computerPlay(){
     return outComes[Math.floor(Math.random() * 3)]
 }
 
-console.log(computerPlay());
+function play(conputerSelection, playerSelection){
+    let result = null;
+    let winner = null;
+    let loser = null;
+    if(conputerSelection().toLowerCase() == playerSelection.toLowerCase()){
+        return "draw"
+    }
+
+    if(playerSelection.toLowerCase() == "rock" && conputerSelection().toLowerCase() == "scissors"){
+        winner = playerSelection;
+        loser = conputerSelection();
+        result = "win";
+    }else if(playerSelection.toLowerCase() == "paper" && conputerSelection().toLowerCase() == "rock"){
+        winner = playerSelection;
+        loser = conputerSelection();
+        result = "win";
+    }else if(playerSelection.toLowerCase() == "scissors" && conputerSelection().toLowerCase() == "paper"){
+        winner = playerSelection;
+        loser = conputerSelection();
+        result = "win";
+    }else{
+        winner = conputerSelection();
+        loser = playerSelection;
+        result = "lose";
+    }
+    return  `You ${result}! ${winner} beats ${playerSelection}`
+}
+
+console.log(play(computerPlay, "rock"));
